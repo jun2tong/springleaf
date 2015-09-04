ceb11e38c3e5b91631b45781d3e5fbee08e333fd
gxyau <guoxian.yau@gmail.com>
2015-09-03 22:19:36 -0400

    Updated source.

--- source.R
+++ source.R
@@ -8,9 +8,9 @@ remove.na <- function(data){
     data <- as.matrix(data)
     nobs <- nrow(data); np <- ncol(data);
     index <- integer(0);
-    for(i in 1:nobs){
-        row <- data[i,]
-        if(any(is.na(row))){
+    for(i in 1:np){
+        col <- data[,i]
+        if(any(is.na(col))){
             index <- c(index,i)
         } else {
             next
